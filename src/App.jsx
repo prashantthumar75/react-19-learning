fdfef81844851ae0d3fd83ee99f79d003a6e47c7
prashantthumar75 <48546055+prashantthumar75@users.noreply.github.com>
2024-09-20 11:51:04 +0530

    [ADDED] Event handler with button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import './components/Netflix.css';
 import NetflixSeries from "./components/NetflixSeries";
-
+import { EventHandling } from './components/EventHandling';
 function App() {
   return (
     <>
       <section className="container">
         <h1 className="card-heading"> List of Best Nextflix series</h1>
-        <NetflixSeries />;
+        <EventHandling></EventHandling>
+        {/* <NetflixSeries />; */}
       </section>
     </>
   );
